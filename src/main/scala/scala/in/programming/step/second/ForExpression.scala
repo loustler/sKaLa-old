276e0adf5720fe018c69cb84b9b2683157b39376
loustler <dev.loustler@gmail.com>
2017-01-14 17:58:34 +0900

    Compare Functional Programming with Imperative Programming
    - Simple example

---
+++ src/main/scala/scala/in/programming/step/second/ForExpression.scala
@@ -0,0 +1,39 @@
+package scala.in.programming.step.second
+
+/**
+  * @author loustler
+  * @since 01/14/2017 17:25
+  */
+object ForExpression {
+
+  /**
+    * Imperative Programming Language Style.
+    *
+    * @param args strings for print
+    */
+  def printArgsI(args: Array[String]): Unit = {
+    var i = 0;
+
+    while ( i < args.length) {
+      println(args(i))
+      i += 1
+    }
+  }
+
+  /**
+    * Function Programming Language Style.
+    *
+    * @param args strings for print
+    */
+  def printArgsF(args: Array[String]): Unit = {
+    for ( arg <- args)
+      println(arg)
+  }
+
+  /**
+    * Function Programming Language Style 2.
+    *
+    * @param args strings for print
+    */
+  def printArgsF2(args: Array[String]): Unit = args.foreach(println)
+}
---
+++ src/main/scala/scala/in/programming/step/second/FunctionExpression.scala
@@ -0,0 +1,50 @@
+package scala.in.programming.step.second
+
+/**
+  * Learn function expression, it compare with Imperative Programming Language.
+  *
+  * @author loustler
+  * @since 01/14/2017 17:12
+  */
+object FunctionExpression extends App{
+  val strs : Array[String] = Array("Hi,", "My", "nickname", "is", "loustler")
+
+  // Imperative Programming Language's for expression.
+  println("------ for :: Imperative Language ------")
+  ForExpression.printArgsI(strs)
+  println("------ for :: Imperative Language ------")
+
+
+  // Functional Programming Language's for expressions. But it has side-effects.
+  println("------ for :: Functional Language ------")
+  ForExpression.printArgsF(strs)
+  println("------ for :: Functional Language 1 ------")
+  ForExpression.printArgsF2(strs)
+  println("------ for :: Functional Language 2 ------")
+
+  /**
+    * Perfectly Functional Programming's function style like ...
+    *
+    * No return value.(Unit type)
+    *
+    * @param args The target string which insert new line.
+    * @return Insert new line into each elements in args.
+    */
+  def formatArgs(args: Array[String]) : String = formatArgs(args, "\n")
+
+  /**
+    * Make string into parameter each element in arguments.
+    *
+    * @param args The target string which insert some string.
+    * @param str Insert string.
+    * @return Insert new string into each elements in args.
+    */
+  def formatArgs(args: Array[String], str: String) : String =
+    args.mkString(str)
+
+
+  println("------ format String 1 ------")
+  println(formatArgs(strs))
+  println("------ format String 2 ------")
+  println(formatArgs(strs, " "))
+}
