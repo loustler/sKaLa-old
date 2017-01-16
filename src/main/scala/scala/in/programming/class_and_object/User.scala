82439315f64ce67d4d0ba70da91e904cd5df0cd8
loustler <dev.loustler@gmail.com>
2017-01-17 02:21:29 +0900

    Modify example

--- src/main/scala/scala/in/programming/class_and_object/User.scala
+++ src/main/scala/scala/in/programming/class_and_object/User.scala
@@ -3,22 +3,26 @@ package scala.in.programming.class_and_object
 /**
   * Very simple example for me
   *
+  * setAge and setName same return type(Unit)
+  *
   * @author loustler
   * @since 01/17/2017 02:05
   */
 class User {
-  private var name: String = "";
-  private var age: Int = 0;
+  private var name: String = ""
+  private var age: Int = 0
 
-  def getAge(): Int = this.age;
+  def getAge(): Int = this.age
 
-  def setAge(age: Int) = this.age = age;
+  def setAge(age: Int) = this.age = age
 
-  def getName(): String = this.name;
+  def getName(): String = this.name
 
-  def setName(name: String) = this.name = name;
+  def setName(name: String) {
+    this.name = name
+  }
 
   override def toString: String = {
-    return " age : " + this.age.toString + ", name : " + this.name;
+    return " age : " + this.age.toString + ", name : " + this.name
   }
 }
