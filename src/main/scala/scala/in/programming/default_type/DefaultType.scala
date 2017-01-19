302abfea4baa31a0ee05f61a9633d68b55e86e5d
loustler <dev.loustler@gmail.com>
2017-01-20 01:24:09 +0900

    Add Default Data Type in Scala

---
+++ src/main/scala/scala/in/programming/default_type/DefaultType.scala
@@ -0,0 +1,17 @@
+package scala.in.programming.default_type
+
+/**
+  * @author loustler
+  * @since 01/20/2017 01:03
+  */
+object DefaultType {
+  val byteMaxHex: Byte = 0x7F
+  val shortMaxHex: Short = 0x7FFF
+  val intMaxHex: Int = 0x7FFFFFFF
+  val longMaxHex: Long = 0x7FFFFFFF
+  val charMaxHex: Char = 0xFFFF
+  val strs: String = ""
+  val floatMax: Float = 3.4028235E38F
+  val doubleMax: Double = 1.7976931348623157E308
+  val booleen: Boolean = true
+}
