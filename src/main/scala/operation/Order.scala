e7663f2da5be57107e88a4b04a220b5bf401427a
loustler <dev.loustler@gmail.com>
2017-01-20 21:06:36 +0900

    Add simple example for order of operation
    - plus and asterisk

---
+++ src/main/scala/operation/Order.scala
@@ -0,0 +1,24 @@
+package operation
+
+import java.lang.AssertionError
+
+/**
+  * @author loustler
+  * @since 01/20/2017 20:51
+  */
+object Order extends App{
+  assert( ("Hello" + " World") == "Hello World", "print msg when assertion fail")
+
+  assert( 2 + 5 * 3 == 17)
+
+  try {
+    assert( 2 + 5 * 3 == 13, "Fail")
+  } catch {
+
+    case e: AssertionError => println(e.getMessage)
+  } finally {
+    println("Catch Error")
+  }
+
+
+}
