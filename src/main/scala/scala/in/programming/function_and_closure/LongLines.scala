745a09757f544ac4ec27522041b0925f07c987f0
loustler <dev.loustler@gmail.com>
2017-02-03 21:50:52 +0900

    Add simple example for function literal

--- src/main/scala/scala/in/programming/function_and_closure/LongLines.scala
+++ src/main/scala/scala/in/programming/function_and_closure/LongLines.scala
@@ -10,19 +10,41 @@ object LongLines {
   def processFile(filename: String, width: Int): Unit = {
     val source = Source.fromFile(filename)
 
-    for (line <- source.getLines())
-     processLine(filename, width, line)
+    for ( line <- source.getLines() )
+      processLine(filename, width, line)
   }
 
   def processLine(filename: String, width: Int, line: String): Unit = {
-    if (line.length > width)
+    if ( line.length > width )
       println(filename + " : " + line.trim)
+  }
+
+  /**
+    * This function use local function literal.
+    *
+    * This function same work [[LongLines.processFile()]] and [[LongLines.processLine()]] perfectly.
+    *
+    * The difference thing just use function literal used.
+    *
+    * @param filename
+    * @param width
+    */
+  def processFileByLiteral(filename: String, width: Int): Unit = {
+    def processLine(filename: String, width: Int, line: String): Unit = {
+      if ( line.length > width )
+        println(filename + " : " + line.trim)
+    }
+
+    val source = Source.fromFile(filename)
+
+    for ( line <- source.getLines() )
+      processLine(filename, width, line)
   }
 
   def main(args: Array[String]): Unit = {
     val width = args(0).toInt
 
-    for (arg <- args.drop(1))
+    for ( arg <- args.drop(1) )
       LongLines.processFile(arg, width)
   }
 }
