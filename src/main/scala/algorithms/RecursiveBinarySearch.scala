07e8945da1d762f85cf3e140332099a4a22248b0
loustler <dev.loustler@gmail.com>
2017-02-09 00:49:35 +0900

    Add a Recursive Binary Search algorithm

--- src/main/scala/algorithms/RecursiveBinarySearch.scala
+++ src/main/scala/algorithms/RecursiveBinarySearch.scala
@@ -4,6 +4,20 @@ package algorithms
   * @author loustler
   * @since 02/09/2017 00:24
   */
-class RecursiveBinarySearch {
+object RecursiveBinarySearch {
+  def binarySearch(array: Array[Int], first: Int, last: Int, target: Int): Int = {
+    if (first > last) -1
 
+    val mid = (first + last) / 2
+
+    if (array(mid) == target) mid
+    else if (array(mid) > target) binarySearch(array, first, mid - 1, target)
+    else binarySearch(array, mid + 1, last, target)
+  }
+
+  def binarySearch2(array: Array[Int], first: Int, last: Int, target: Int): Int =
+    if (first > last) -1
+    else if (array((first + last) / 2) == target) (first + last) / 2
+    else if (array((first + last) / 2) > target) binarySearch2(array, first, ((first + last) / 2) - 1, target)
+    else binarySearch2(array, ((first + last) / 2) + 1, last, target)
 }
--- src/test/scala/algorithms/RecursiveBinarySearchTest.scala
+++ src/test/scala/algorithms/RecursiveBinarySearchTest.scala
@@ -4,6 +4,17 @@ package algorithms
   * @author loustler
   * @since 02/09/2017 00:33
   */
-object RecursiveBinarySearchTest {
+object RecursiveBinarySearchTest extends App{
+  val numberList = Array(1, 2, 3, 4, 5, 6, 7, 10, 15, 20)
+  val start = 0
+  val last = numberList.length - 1
+  val find = 15
+  val findIndex = 8
+
+  val result1 = RecursiveBinarySearch binarySearch(numberList, start, last, find)
+  val result2 = RecursiveBinarySearch binarySearch2(numberList, start, last, find)
+
+  assert(result1 == findIndex)
+  assert(result2 == findIndex)
 
 }
