15ad96e3c5346ab8a3981e8e4257f372cfcb9ff8
loustler <dev.loustler@gmail.com>
2017-03-02 01:57:17 +0900

    Add example for inheritance and composition part 2.

---
+++ src/main/scala/scala/in/programming/inheritance_composition/LineElement.scala
@@ -0,0 +1,12 @@
+package scala.in.programming.inheritance_composition
+
+/**
+  * {{{Array(s)}}} is called super class' constructor.
+  *
+  * @author loustler
+  * @since 03/02/2017 01:07
+  */
+class LineElement(s: String) extends ArrayElement(Array(s)){
+  override def width = s.length
+  override def height = 1
+}
