9b7f34df9afef8ac85c013fbd49bd50e274b27e9
loustler <dev.loustler@gmail.com>
2017-03-15 00:41:04 +0900

    #beside change to style of imperative to style of functional.

--- src/main/scala/scala/in/programming/inheritance_composition/Element.scala
+++ src/main/scala/scala/in/programming/inheritance_composition/Element.scala
@@ -18,7 +18,7 @@ abstract class Element {
 
 
   /**
-    * [[scala.collection.TraversableLike.++]]
+    * [[scala.collection.TraversableLike#++]]
     * Returns this contents merge another element
     *
     * @param that Another element to merge.
@@ -31,13 +31,30 @@ abstract class Element {
   /**
     * Append this contents and another contents.
     *
+    * [[scala.collection.IndexedSeqOptimized#zip]] that aggregates the contents
+    * of two lists into a single list of pairs.
+    * {{{
+    *   scala> Array(1, 2, 3) zip Array("a", "b")
+    *   => Array((1, "a"), (2, "b"))
+    *
+    *   scala> List(1, 2, 3) zip List( "a", "b", "c")
+    *   => List((1, "a"), (2, "b"), (3, "c"))
+    * }}}
+    *
     * @param that
     * @return
     */
   def beside(that: Element): Element = {
+    /*
     val contents = new Array[String](this.contents.length)
     for ( i <- 0 until this.contents.length )
       contents(i) = this.contents(i) + that.contents(i)
     new ArrayElement(contents)
+    */
+    new ArrayElement(
+      for (
+        (line1, line2) <- this.contents zip that.contents
+      ) yield line1 + line2
+    )
   }
 }
