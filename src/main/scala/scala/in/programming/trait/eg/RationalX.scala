4f3cfb432dddc1c104b4e32ff33e344fc2f9f9e8
loustler <dev.loustler@gmail.com>
2017-03-24 21:01:33 +0900

    Add simple example when trait of Ordered not exist.

---
+++ src/main/scala/scala/in/programming/trait/eg/RationalX.scala
@@ -0,0 +1,19 @@
+package scala.in.programming.`trait`.eg
+
+import scala.in.programming.functional_object.Rational
+
+/**
+  * @author loustler
+  * @since 03/24/2017 20:56
+  */
+class RationalX(n: Int, d: Int) extends Rational(n, d){
+  def < (that: RationalX) =
+   this.numer * that.denom > that.numer * this.denom
+
+  def > (that: RationalX) =
+   that < this
+
+  def <= (that: RationalX) = (this < that) || (this == that)
+
+  def >= (that: RationalX) = (this > that) || (this == that)
+}
