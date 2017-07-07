5796b4631b8a020beda7b3062d486135126533e8
loustler <dev.loustler@gmail.com>
2017-07-07 20:54:35 +0900

    Fix error when build on travis-ci for testing.

---
+++ build/build.sh
@@ -0,0 +1,18 @@
+# For JDK on OSX
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	/usr/libexec/java_home -V
+	
+	brew update
+
+	brew outdated caskroom/cask/brew-cask || brew upgrade caskroom/cask/brew-cask
+
+	brew tap caskroom/versions
+	
+	sudo rm -rf /Library/Java/JavaVirtualMachines
+	
+	brew cask install caskroom/versions/java8
+
+	/usr/libexec/java_home --failfast --version 1.8
+else
+	jdk_switcher use oraclejdk8
+fi
